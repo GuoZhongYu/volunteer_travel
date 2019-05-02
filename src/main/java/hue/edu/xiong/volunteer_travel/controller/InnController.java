f49e3cc9561f97e40ccc9bb8e69590268bf9c235
miyahejuzi <2426105329@qq.com>
2019-05-02 20:41:50 +0800

    首页栏目的修改

--- src/main/java/hue/edu/xiong/volunteer_travel/controller/InnController.java
+++ src/main/java/hue/edu/xiong/volunteer_travel/controller/InnController.java
@@ -1,7 +1,10 @@
 package hue.edu.xiong.volunteer_travel.controller;
 
+import hue.edu.xiong.volunteer_travel.core.Result;
+import hue.edu.xiong.volunteer_travel.core.ResultGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Author Xiong YuSong
@@ -10,8 +13,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class InnController {
 
+    /**
+     * 客栈列表展示
+     * @return
+     */
     @RequestMapping("/innIndexUI")
     public String index(){
         return "inn/inn-index";
     }
+
+    /**
+     * 客栈注册页面
+     * @return
+     */
+    @RequestMapping("/innRegisterUI")
+    public String innRegisterUI(){
+        return "inn/inn-register";
+    }
+
+    /**
+     * 客栈添加.保存
+     */
+    @ResponseBody
+    @RequestMapping("/innSave")
+    public Result innSave(){
+        return ResultGenerator.genSuccessResult();
+    }
+
+
 }
